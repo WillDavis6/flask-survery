2227698ac93d0769f68bdf9837856b776009430e
William <mailliwrd@gmail.com>
2024-01-27 09:18:58 -0700

    bulk of survey assignment

--- app.py
+++ app.py
@@ -1,4 +1,48 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
-app.flask 
+RESPONSES_KEY = "responses"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] ='secret'
+
+debug = DebugToolbarExtension(app)
+
+response = []
+
+@app.route("/")
+def show_survey_start():
+    """Select a survey"""
+    return render_template("survery_start.html", survery=survey)
+
+@app.route("/begin", methods=['POST'])
+def start_survey():
+    """Clear the session of responses"""
+
+    session[RESPONSES_KEY] = []
+
+    return redirect("/questions/0")
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save the responses and redirect to next question"""
+    
+    #get the response choice
+    choice = request.form('answer')
+
+    #add this response to the session
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+    
+@app.route("/questions/<int:qid>")
+def complete():
+    "surcey complete. show completion page"
+    return render_template("completion.html")
